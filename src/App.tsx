bbe2803b0c39f766ca520f3132b2604b2080b293
qkrekdls <gunmannduu@gmail.com>
2026-07-07 15:31:16 +0900

    feat: LoadingPage UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from '@/pages/MainPage';
+import LoadingPage from '@/pages/draw/LoadingPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/draw/loading" element={<LoadingPage />} />
       </Routes>
     </BrowserRouter>
   );
