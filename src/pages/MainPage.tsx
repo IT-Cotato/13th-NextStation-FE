44eea22cd3986ebff422617424103225033e1d94
qkrekdls <gunmannduu@gmail.com>
2026-07-07 16:02:55 +0900

    feat: Button component & refactoring main page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,10 +3,9 @@ import ArrowLeft from '@/assets/arrow-l.svg?react';
 import ArrowRight from '@/assets/arrow-r.svg?react';
 import DoorLeft from '@/assets/door_L.svg?react';
 import DoorRight from '@/assets/door_R.svg?react';
-import ArrowPrev from '@/assets/arrow-prev.svg?react';
-import ArrowNext from '@/assets/arrow-next.svg?react';
 import ProfileIcon from '@/components/ProfileIcon';
 import { useNavigate } from 'react-router-dom';
+import Button from '@/components/Button';
 
 function MainPage() {
   const navigate = useNavigate();
@@ -40,38 +39,24 @@ function MainPage() {
         {/* 왼쪽 문 */}
         <div className="relative h-full w-1/2">
           <DoorLeft className="h-full w-full" />
-          <button
-            type="button"
+          <Button
+            direction="left"
             onClick={() => navigate('/draw/loading')}
-            className='
-              absolute left-5 top-[56%] z-10 flex items-center justify-center gap-2 px-6 py-4 rounded-full 
-              bg-linear-to-r from-primary-50 to-secondary-50 border-[2px] border-secondary-30 
-              shadow-[0_0_8px_var(--color-secondary-50)]
-              active:scale-[0.96]
-              active:from-[#EF9E8C] active:to-[#E5989F] active:border-secondary-50
-            '
+            className="absolute left-5 top-[56%] z-10"
           >
-            <ArrowPrev className='size-6'/>
-            <h2 className='whitespace-nowrap text-title-01 font-semibold text-gray-10 leading-5 tracking-[-0.025em]'>랜덤뽑기</h2>
-          </button>
+            랜덤뽑기
+          </Button>
         </div>
         {/* 오른쪽 문 */}
         <div className="relative h-full w-1/2">
           <DoorRight className="h-full w-full" />
-          <button
-            type="button"
+          <Button
+            direction="right"
             onClick={() => navigate('/draw/recommend')}
-            className='
-              absolute right-5 top-[56%] z-10 flex items-center justify-center gap-2 px-6 py-4 rounded-full
-              bg-linear-to-r from-secondary-50 to-primary-50 border-[2px] border-secondary-30
-              shadow-[0_0_8px_var(--color-secondary-50)]
-              active:scale-[0.96]
-              active:from-[#EF9E8C] active:to-[#E5989F] active:border-secondary-50
-            '
+            className="absolute right-5 top-[56%] z-10"
           >
-            <h2 className='whitespace-nowrap text-title-01 font-semibold text-gray-10 leading-5 tracking-[-0.025em]'>맞춤추천</h2>
-            <ArrowNext className='size-6'/>
-          </button>
+            맞춤추천
+          </Button>
         </div>
       </div>      
     </section>
