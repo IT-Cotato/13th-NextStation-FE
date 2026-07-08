fa224fd70258e658270677c4277b64481c3c2edc
qkrekdls <gunmannduu@gmail.com>
2026-07-08 14:40:33 +0900

    feat: BackHeader Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from '@/pages/MainPage';
 import LoadingPage from '@/pages/draw/LoadingPage';
 import ResultPage from '@/pages/draw/ResultPage';
+import RecommendPage from './pages/draw/RecommendPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/draw/loading" element={<LoadingPage />} />
         <Route path="/draw/result" element={<ResultPage />} />
+        <Route path="/draw/recommend" element={<RecommendPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/draw/RecommendPage.tsx
@@ -0,0 +1,11 @@
+import BackHeader from "@/components/BackHeader"
+
+function RecommendPage() {
+  return (
+    <main className="flex flex-col h-dvh overflow-hidden bg-gray-10 items-center pt-[var(--safe-top)]">
+      <BackHeader/>
+    </main>
+  )
+}
+
+export default RecommendPage
