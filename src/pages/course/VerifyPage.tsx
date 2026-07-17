df3d7d0029bca42db38a0c66c25f8727d9b62796
Eunjin-n <ikee1652@duksung.ac.kr>
2026-07-17 14:30:05 +0900

    feat: 코스 확인 페이지

---
+++ src/pages/course/VerifyPage.tsx
@@ -0,0 +1,171 @@
+import EditPen from "@/assets/edit-pen.svg?react";
+import MapIcon from "@/assets/map-icon.svg?react";
+import Button from "@/components/Button";
+import CourseCard from "@/pages/course/components/CourseCard";
+import CourseNumber from "@/pages/course/components/CourseNumber";
+import Header from "@/components/Header";
+import StationTitle from "@/components/StationTitle";
+import { mockCourses } from "@/mocks/mockCourses";
+import { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { CustomOverlayMap, Map, useKakaoLoader } from "react-kakao-maps-sdk";
+import MapMarker from "./components/MapMarker";
+import { Reorder } from "motion/react";
+import ExitConfirmModal from "./components/ExitConfirmModal";
+
+export default function VerifyPage() {
+  const navigate = useNavigate();
+  const [isNameEditing, setIsNameEditing] = useState(false);
+  const [courseName, setCourseName] = useState("보문역 환승여행 코스");
+  const [namePlaceholder, setNamePlaceholder] = useState("");
+  const nameInputRef = useRef<HTMLInputElement>(null);
+  const courseListRef = useRef<HTMLUListElement>(null);
+  const [courses, setCourses] = useState(mockCourses); // 초기값 : mockCourses
+  const [pressedId, setPressedId] = useState<number | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleNameEdit = () => {
+    setIsNameEditing((prev) => {
+      const next = !prev;
+      if (next) setNamePlaceholder(courseName);
+      return next;
+    });
+  };
+
+  useEffect(() => {
+    if (isNameEditing) {
+      nameInputRef.current?.focus();
+      nameInputRef.current?.select();
+    }
+  }, [isNameEditing]);
+
+  const [loading, error] = useKakaoLoader({
+    appkey: import.meta.env.VITE_KAKAO_API,
+  });
+
+  if (loading || error) {
+    return (
+      <div>{error ? "지도를 불러오지 못했어요." : "지도를 불러오는 중..."}</div>
+    );
+  }
+
+  const toggleModal = () => setIsModalOpen((prev) => !prev);
+
+  return (
+    <main className="flex flex-col h-dvh overflow-hidden bg-gray-10 gap-8 pt-[calc(var(--safe-top)+12px)]">
+      <Header showBack showClose onCloseClick={toggleModal} />
+
+      {/* 경고 모달 */}
+      {isModalOpen && <ExitConfirmModal onClose={toggleModal} />}
+
+      {/* 역명 */}
+      <section className="flex justify-center">
+        <StationTitle line={6} stationName="보문역" />
+      </section>
+
+      {/* 지도 */}
+      <section className="flex justify-center">
+        <div className="flex flex-col gap-2.5 w-[360px]">
+          <div className="flex relative items-center border border-gray-40 bg-white rounded-[20px] p-2.5 caret-primary-50">
+            <input
+              className="w-full text-center outline-none text-subtitle font-semibold placeholder:text-gray-50"
+              ref={nameInputRef}
+              value={courseName}
+              placeholder={namePlaceholder}
+              onChange={(e) => setCourseName(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") setIsNameEditing(false);
+              }}
+            />
+            <div className="absolute right-3" onClick={handleNameEdit}>
+              <EditPen className="w-6 h-6 text-gray-70" />
+            </div>
+          </div>
+
+          <div className="flex flex-col border h-[357px] border-gray-40 bg-white rounded-[20px] overflow-hidden">
+            <div className="flex items-center justify-between px-4 py-4">
+              <p className="text-title-02 font-semibold">내 코스 지도</p>
+              <MapIcon className="w-6 h-6 text-gray-70 cursor-pointer" />
+            </div>
+            <Map
+              center={{ lat: 37.585, lng: 127.0192 }}
+              level={5}
+              style={{ width: "100%", height: "300px" }}
+            >
+              {courses.map((course, i) => (
+                <CustomOverlayMap
+                  key={course.id}
+                  position={course.position}
+                  yAnchor={1}
+                >
+                  <MapMarker number={i + 1} />
+                </CustomOverlayMap>
+              ))}
+            </Map>
+          </div>
+        </div>
+      </section>
+
+      {/* 코스 순서 */}
+      <section className="flex flex-col gap-4 items-center">
+        <div className="flex flex-col gap-1 w-[360px]">
+          <p className="text-title-02 font-semibold text-gray-100">
+            내 코스 순서
+          </p>
+          <p className="text-body-02 text-gray-70">
+            장소를 길게 누르면 순서를 변경할 수 있어요!
+          </p>
+        </div>
+
+        {/* 번호 + 카드 하나 */}
+        <Reorder.Group
+          as="ul"
+          ref={courseListRef}
+          axis="y"
+          values={courses}
+          onReorder={setCourses}
+          className="flex flex-col gap-[13px] "
+        >
+          {courses.map((course, index) => (
+            <Reorder.Item
+              key={course.id}
+              value={course}
+              dragConstraints={courseListRef}
+              dragElastic={0.05}
+              className="flex items-center gap-[13px]"
+              onPointerDown={() => setPressedId(course.id)}
+              onPointerUp={() => setPressedId(null)}
+              onPointerLeave={() => setPressedId(null)}
+            >
+              <CourseNumber number={index + 1} />
+              <CourseCard
+                name={course.name}
+                description={course.description}
+                category={course.category}
+                isActive={pressedId === course.id}
+              />
+            </Reorder.Item>
+          ))}
+        </Reorder.Group>
+      </section>
+
+      {/* 하단 버튼 */}
+      <section className="flex justify-center">
+        <div className="flex w-[360px] justify-between">
+          <Button
+            direction="left" // 왼쪽 화살표가 있는 버튼
+            onClick={() => navigate("/~")}
+          >
+            다시 뽑기
+          </Button>
+          <Button
+            direction="right" // 오른쪽 화살표가 있는 버튼
+            onClick={() => navigate("/")}
+          >
+            코스 저장
+          </Button>
+        </div>
+      </section>
+    </main>
+  );
+}
