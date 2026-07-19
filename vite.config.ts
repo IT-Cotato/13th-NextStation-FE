27a01a321fc2cfe6c5cbbb158e6bdc069f569f32
Eunjin-n <ikee1652@duksung.ac.kr>
2026-07-19 23:59:36 +0900

    chore: HTTPS 로컬 테스트용 터널 허용 호스트 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import svgr from 'vite-plugin-svgr'
-import path from 'node:path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    svgr(),
-  ],
+  plugins: [react(), tailwindcss(), svgr()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+
+  // https 환경에서 공유하기 기능 확인 위함
+  server: {
+    allowedHosts: [".loca.lt", ".trycloudflare.com"],
+  },
+  preview: {
+    allowedHosts: [".loca.lt", ".trycloudflare.com"],
+  },
+});
