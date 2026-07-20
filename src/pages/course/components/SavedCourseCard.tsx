038e41cdfde0b6021ef0e7515b183e9a42aa3033
Eunjin-n <ikee1652@duksung.ac.kr>
2026-07-20 17:25:05 +0900

    feat: 저장된 코스 카드 컴포넌트 추가

---
+++ src/pages/course/components/SavedCourseCard.tsx
@@ -0,0 +1,68 @@
+import CardActionButton from "./CardActionButton";
+import { useNavigate } from "react-router-dom";
+import Checked from "@/assets/checked.svg?react";
+import Unchecked from "@/assets/unchecked.svg?react";
+import { STATION_STAMP_MAP } from "@/constants/stationStampMap";
+import { DISABLED_LINE_STAMP_MAP } from "@/constants/disabledLineStampMap";
+
+export default function SavedCourseCard({
+  id,
+  name,
+  line,
+  stationName,
+  isCourseCompleted,
+  onCompletedClick,
+  isDeleteMode,
+  isSelect,
+  handleSelected,
+}: {
+  id: number;
+  name: string;
+  line: string;
+  stationName: string;
+  isCourseCompleted: boolean;
+  onCompletedClick: () => void;
+  isDeleteMode: boolean;
+  isSelect: boolean;
+  handleSelected: (targetId: number) => void;
+}) {
+  const navigate = useNavigate();
+  const StampIcon = isCourseCompleted
+    ? STATION_STAMP_MAP[stationName]
+    : DISABLED_LINE_STAMP_MAP[line];
+
+  return (
+    <div
+      className={`${isSelect ? "border border-primary-50 bg-secondary-10" : "border border-white bg-white"} flex w-[361px] p-3 gap-4 rounded-lg`}
+      onClick={() => {
+        if (isDeleteMode) handleSelected(id);
+      }}
+    >
+      <div>{StampIcon ? <StampIcon className="w-16 h-16" /> : null}</div>
+      <div className="flex flex-col flex-1 gap-2.5">
+        <span className="text-black text-subtitle font-semibold">{name}</span>
+        <div className="flex gap-1">
+          <CardActionButton
+            onClick={() => {
+              if (!isDeleteMode) navigate("/course/verify");
+            }}
+          >
+            코스 확인
+          </CardActionButton>
+          <CardActionButton
+            onClick={() => {
+              if (!isDeleteMode) onCompletedClick();
+            }}
+          >
+            여행 완료
+          </CardActionButton>
+        </div>
+      </div>
+      {isDeleteMode && (
+        <div className="flex items-center">
+          {isSelect ? <Checked /> : <Unchecked />}
+        </div>
+      )}
+    </div>
+  );
+}
