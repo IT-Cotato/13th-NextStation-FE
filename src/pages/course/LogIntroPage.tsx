b282e061f0d6fb909f90f0a8d857eeb2d8bde5d7
qkrekdls <gunmannduu@gmail.com>
2026-07-23 15:44:37 +0900

    refactor: 페이지 전체 레이아웃 리팩토링

--- src/pages/course/LogIntroPage.tsx
+++ src/pages/course/LogIntroPage.tsx
@@ -8,30 +8,31 @@ function LogIntroPage() {
 
   return (
     <main className="flex flex-col h-dvh overflow-hidden bg-white items-center pt-[var(--safe-top)]">
-      <section className="flex flex-col h-full items-center pt-[200px] gap-[60px]">
-        <div className="flex flex-col items-center justify-center gap-4">
-          {/* 역이름 변경 필요 */}
-          <h1 className="text-headline font-semibold text-gray-100 leading-[1.4] tracking-[-0.025em] text-center">
-            오늘의 보문역을<br />
-            조금 더 오래 기억해볼까요?
-          </h1>
-          <p className="text-body-01 text-gray-70 leading-[1.4] tracking-[-0.025em] text-center">
-            사진과 한 줄 감상을 남기면<br />
-            스탬프 뒷면에 오늘의 여행이 저장돼요.
-          </p>
+      <section className="flex h-full flex-col items-center justify-between pt-[100px] pb-[calc(var(--safe-bottom)+10px)]">
+        <div className="flex flex-col items-center gap-[60px]">
+          <div className="flex flex-col items-center justify-center gap-4">
+            {/* 역이름 변경 필요 */}
+            <h1 className="text-headline font-semibold text-gray-100 leading-[1.4] tracking-[-0.025em] text-center">
+              오늘의 보문역을<br />
+              조금 더 오래 기억해볼까요?
+            </h1>
+            <p className="text-body-01 text-gray-70 leading-[1.4] tracking-[-0.025em] text-center">
+              사진과 한 줄 감상을 남기면<br />
+              스탬프 뒷면에 오늘의 여행이 저장돼요.
+            </p>
+          </div>
+          <LogIcon className="w-[197px]"/>
         </div>
-        <LogIcon className="w-[197px]"/>
 
-      </section>
-
-      {/* CTA Button */}
-      <section className="absolute bottom-[calc(var(--safe-bottom)+50px)] z-10 flex flex-col w-full items-center justify-center gap-[10px]">
-        <CTAButton onClick={() => navigate(`/course/${courseId}/log/info`)}>
-          환승여행 기록하기
-        </CTAButton>
-        <CTAButton variant="secondary" onClick={() => navigate('/course')}>
-          다음에 할게요
-        </CTAButton>
+        {/* CTA Button */}
+        <section className="flex flex-col w-full items-center justify-center gap-[10px]">
+          <CTAButton onClick={() => navigate(`/course/${courseId}/log/info`)}>
+            환승여행 기록하기
+          </CTAButton>
+          <CTAButton variant="secondary" onClick={() => navigate('/course')}>
+            다음에 할게요
+          </CTAButton>
+        </section>
       </section>
     </main>
   )
