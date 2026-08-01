e1bbf1285324560476bb060ef07fd45f2b98246f
Eunjin-n <ikee1652@duksung.ac.kr>
2026-08-02 03:57:53 +0900

    fix: 공유 시트 동작 수정

--- src/pages/course/SavedPage.tsx
+++ src/pages/course/SavedPage.tsx
@@ -1,18 +1,28 @@
 import CTAButton from "@/components/CTAButton";
 import Header from "@/components/Header";
 import CourseSaved from "@/assets/course-saved.svg?react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import share from "@/utils/share";
 import { showToast } from "./components/ShowToast";
 
+interface SavedCourseState {
+  courseName: string;
+  courseId: number | null;
+}
+
 export default function SavedPage() {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const courseInfo = location.state as SavedCourseState | null;
 
   const handleShareClick = async () => {
+    if (!courseInfo) return;
+
     const result = await share({
       title: "환승여행",
-      text: "보문역 환승여행 코스를 확인해보세요!",
-      url: "http://172.30.1.48:5173/course/verify",
+      text: `${courseInfo.courseName} 코스를 확인해보세요!`,
+      url: `http://172.30.1.48:5173/course/${courseInfo.courseId}/verify`,
     });
 
     if (result === "copiedToClipboard") {
@@ -30,7 +40,7 @@ export default function SavedPage() {
       <section className="flex flex-1 items-center">
         <div className="flex flex-col items-center justify-center gap-8">
           <CourseSaved className="w-[130px] h-[130px]" />
-          <p className="text-gray-100 text-headline font-semibold text-center">
+          <p className="text-gray-100 text-headline font-semibold text-center leading-[1.4] tracking-[-0.6px]">
             보관함에
             <br />
             저장이 완료됐어요!
@@ -39,11 +49,13 @@ export default function SavedPage() {
       </section>
 
       <section className="flex flex-col gap-4 pb-[10px]">
-        <span className="text-gray-70 text-body-01 text-center">
+        <span className="text-gray-70 text-body-01 text-center leading-[1.4] tracking-[-0.35px]">
           같이 떠날 친구에게 코스를 공유해 보세요!
         </span>
         <div className="flex flex-col gap-[10px]">
-          <CTAButton onClick={handleShareClick}>공유하기</CTAButton>
+          <CTAButton mode="share" onClick={handleShareClick}>
+            공유하기
+          </CTAButton>
           <CTAButton variant="secondary" onClick={() => navigate("/course")}>
             보관함으로 이동하기
           </CTAButton>
