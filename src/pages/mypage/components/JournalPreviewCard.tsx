b1e44427250bdc63efd86bc0bec03899d68b0412
qkrekdls <gunmannduu@gmail.com>
2026-08-12 01:16:45 +0900

    refactor: 마이페이지 배경 유무에 따른 text 색상 변경

--- src/pages/mypage/components/JournalPreviewCard.tsx
+++ src/pages/mypage/components/JournalPreviewCard.tsx
@@ -1,6 +1,7 @@
 import CardBG from "@/assets/card-default.svg?react";
 import LineBadge from "@/components/LineBadge";
 import Heart from "@/assets/heart-active.svg?react";
+import WhiteHeart from '@/assets/white-heart-filled.svg?react';
 import type { SubwayLine } from "@/types/subway";
 
 export default function JournalPreviewCard({
@@ -17,6 +18,8 @@ export default function JournalPreviewCard({
   likeCount?: number;
 }) {
   const isThumbnailEmpty = thumbnailUrl === null;
+  const textColorClass = isThumbnailEmpty ? "text-gray-100" : "text-white";
+  const HeartIcon = isThumbnailEmpty ? Heart : WhiteHeart;
 
   return (
     <div className="relative w-[116px] h-40 rounded-lg overflow-hidden">
@@ -26,9 +29,9 @@ export default function JournalPreviewCard({
         <div
           role="img"
           aria-label={journalTitle}
-          className="absolute inset-0 h-full w-full rounded-lg"
+          className="absolute inset-0 h-full w-full rounded-lg text-gray-100"
           style={{
-            background: `linear-gradient(180deg, rgba(255, 255, 255, 0.00) 50%, #FFF 100%), url(${thumbnailUrl}) lightgray 50% / cover no-repeat`,
+            background: `linear-gradient(180deg, rgba(255, 255, 255, 0.00) 50%, #555555 100%), url(${thumbnailUrl}) lightgray 50% / cover no-repeat`,
           }}
         />
       )}
@@ -37,14 +40,14 @@ export default function JournalPreviewCard({
         {/* station info */}
         <div className="flex gap-1 z-10 items-center">
           {lineId !== undefined && <LineBadge line={lineId as SubwayLine} />}
-          <span className="text-body-02 justify-center leading-[1.4] tracking-[-0.3px]">
+          <span className={`text-body-02 justify-center ${textColorClass} leading-[1.4] tracking-[-0.3px]`}>
             {stationName}
           </span>
         </div>
 
         {/* journalName */}
         <div className="flex z-10">
-          <p className="flex text-start text-body-01 font-semibold leading-[1.4] tracking-[-0.35px] line-clamp-2 break-keep">
+          <p className={`flex text-start text-body-01 font-semibold ${textColorClass} leading-[1.4] tracking-[-0.35px] line-clamp-2 break-keep`}>
             {journalTitle}
           </p>
         </div>
@@ -52,8 +55,8 @@ export default function JournalPreviewCard({
         {/* heart */}
         {likeCount !== undefined && (
           <div className="flex z-10 items-center">
-            <Heart />
-            <span className="text-caption leading-none tracking-[-0.25px] text-gray-70">
+            <HeartIcon className="size-3"/>
+            <span className={`text-caption ${textColorClass} leading-none tracking-[-0.25px]`}>
               {likeCount}
             </span>
           </div>
